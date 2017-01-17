5654ecd32285344c1cf98c62707ebae00f11e7dd
bobnewmark <bobnewmark@ukr.net>
2017-01-17 22:29:09 +0200

    set up basic Player class features and created enum Status

--- server/src/main/java/chess/model/Player.java
+++ server/src/main/java/chess/model/Player.java
@@ -4,4 +4,17 @@ package chess.model;
  * Created by Admin on 17.01.2017.
  */
 public class Player {
+    private String login;
+    private String password;
+    private String nickname;
+    private int rank;
+    private Status status;
+
+    public Player(String login, String password, String nickname, int rank) {
+        this.login = login;
+        this.password = password;
+        this.nickname = nickname;
+        this.rank = 50;   // MAGIC NUMBER! NEED TO BE REPLACED BY A CONSTANT
+        this.status = Status.FREE;
+    }
 }
---
+++ server/src/main/java/chess/model/Status.java
@@ -0,0 +1,11 @@
+package chess.model;
+
+/**
+ * Created by bobnewmark on 17.01.2017
+ */
+public enum Status {
+    OFFLINE,
+    FREE,
+    INGAME,
+    BANNED
+}
