628a7c7af4b437b6cf31e72c5578cc28eec700f3
bobnewmark <bobnewmark@ukr.net>
2017-01-18 22:22:43 +0200

    Bishop, Castle and King logics is ready, code still needs to be done yet

--- server/src/main/java/chess/model/figures/King.java
+++ server/src/main/java/chess/model/figures/King.java
@@ -2,8 +2,10 @@ package chess.model.figures;
 
 import chess.model.Cell;
 import chess.model.Figure;
+import chess.model.Game;
 import chess.model.Type;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,6 +21,73 @@ public class King extends Figure {
     }
 
     public List<Cell> allAccessibleMove() {
-        return null;
+        List<Cell> validCells = new ArrayList<Cell>();
+
+        int a = cell.getX();
+        int b = cell.getY();
+
+        /* adding all cells to the validCells list as long as they don't have figures
+        * and as we have a cell with a figure we add it and check another direction */
+        for (int x = a - 1; x <= a + 1; x++) {
+            for (int y = b -1; y <= b + 1; y++) {
+                if (Game.getCell(x,y).getFigure() == this) continue;
+                validCells.add(Game.getCell(x, y));
+            }
+        }
+
+        /* all cells that have figures of the same Type as this are removed here*/
+        for (int i = 0; i < validCells.size(); i++) {
+            if (validCells.get(i).isFriendlyCell(this)) {
+                validCells.remove(i);
+            }
+        }
+        return validCells;
+    }
+
+    // overriding method to specialize castling both sides
+    @Override
+    public void move(Cell destination) {
+        if ((allAccessibleMove().contains(destination)) && hasMove()) {
+            this.cell.setFigure(null);
+            this.cell = destination;
+            this.cell.setFigure(this);
+        } else if ((allAccessibleMove().contains(destination))
+        && (true /*destination is castling Kingside cell*/)) {
+            castlingKingside();
+        } else if ((allAccessibleMove().contains(destination))
+                && (true /*destination is castling Queenside cell*/)) {
+            castlingQueenside();
+        }
+    }
+
+    public boolean castlingKingsideAllowed() {
+        return ((true /*king never moved*/)
+                && (true /*castle never moved*/)
+                && (true /*king is not under attack*/)
+                && (true /*cells for king to pass are safe*/)
+                && (true /*position after castling is safe*/));
+    }
+
+
+    public boolean castlingQueensideAllowed() {
+        return ((true /*king never moved*/)
+                && (true /*castle never moved*/)
+                && (true /*king is not under attack*/)
+                && (true /*cells for king to pass are safe*/)
+                && (true /*position after castling is safe*/));
+    }
+
+    public void castlingKingside() {
+        if (castlingKingsideAllowed()) {
+            this.move(null /*Cell destination*/);
+            /*and castle (by it's unique number) moves too*/
+        }
+    }
+
+    public void castlingQueenside() {
+        if (castlingQueensideAllowed()) {
+            this.move(null /*Cell destination*/);
+            /*and castle (by it's unique number) moves too*/
+        }
     }
 }
