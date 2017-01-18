46ab617b0d01b55973df290bd28251dcf179e277
bobnewmark <bobnewmark@ukr.net>
2017-01-18 20:32:43 +0200

    Bishop and Castle methods allAccessibleMove() are almost done, the math is

--- server/src/main/java/chess/model/figures/Castle.java
+++ server/src/main/java/chess/model/figures/Castle.java
@@ -2,8 +2,10 @@ package chess.model.figures;
 
 import chess.model.Cell;
 import chess.model.Figure;
+import chess.model.Game;
 import chess.model.Type;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,6 +21,40 @@ public class Castle extends Figure {
     }
 
     public List<Cell> allAccessibleMove() {
-        return null;
+        List<Cell> validCells = new ArrayList<Cell>();
+
+        int a = cell.getX();
+        int b = cell.getY();
+
+        /* adding all cells to the validCells list as long as they don't have figures
+        * and as we have a cell with a figure we add it and check another direction */
+        for (int x = a - 1; x >= 0; x--) {
+            do {
+                validCells.add(Game.getCell(x, b));
+            } while (Game.getCell(x, b).getFigure() = null);
+        }
+        for (int x = a + 1; x <= 7; x++) {
+            do {
+                validCells.add(Game.getCell(x, b));
+            } while (Game.getCell(x, b).getFigure() = null);
+        }
+        for (int y = b - 1; y >= 0; y--) {
+            do {
+                validCells.add(Game.getCell(a, y));
+            } while (Game.getCell(a, y).getFigure() = null);
+        }
+        for (int y = b + 1; y <= 7; y++) {
+            do {
+                validCells.add(Game.getCell(a, y));
+            } while (Game.getCell(a, y).getFigure() = null);
+        }
+
+        /* all cells that have figures of the same Type as this are removed here*/
+        for (int i = 0; i < validCells.size(); i++) {
+            if (validCells.get(i).isFriendlyCell(this)) {
+                validCells.remove(i);
+            }
+        }
+        return validCells;
     }
 }
