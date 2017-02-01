bbfc4f466242aef85e465bbcab7b730dc8edc266
bobnewmark <bobnewmark@ukr.net>
2017-02-01 18:30:26 +0200

    drag and drop on GameFrame works fine

--- client/src/main/java/chess/view/GameFrame.java
+++ client/src/main/java/chess/view/GameFrame.java
@@ -1,29 +1,31 @@
 package chess.view;
 
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by bobnewmark on 30.01.2017
  */
 public class GameFrame extends Application {
 
-    @FXML
-    GridPane grid;
-
-    //    public GameFrame() throws IOException {
-//    }
     public static void main(String[] args) {
         launch(args);
     }
@@ -41,73 +43,78 @@ public class GameFrame extends Application {
         window.show();
     }
 
-
     @FXML
-    public void handle(MouseEvent event) {
-        //Drag was detected, start drap-and-drop gesture
-        //Allow any transfer node
+    public void handleOnDrag(MouseEvent event) {
+
+        Node node = (Node) event.getSource();
+        node = node.getParent();
+        System.out.print("Drag from cell: ");
+        System.out.print("X:" + GridPane.getColumnIndex(node));
+        System.out.println(" Y:" + GridPane.getRowIndex(node));
+
         ImageView image = (ImageView) event.getSource();
         Dragboard db = image.startDragAndDrop(TransferMode.ANY);
 
-        //Put ImageView on dragboard
         ClipboardContent cbContent = new ClipboardContent();
         cbContent.putImage(image.getImage());
-        //cbContent.put(DataFormat.)
         db.setContent(cbContent);
         image.setVisible(false);
         event.consume();
     }
 
+    @FXML
+    public void handleDragOver(DragEvent de) {
+        Dragboard board = de.getDragboard();
+        if (board.hasImage()) {
+            de.acceptTransferModes(TransferMode.ANY);
+        }
+    }
 
     @FXML
     public void handleDrop(DragEvent event) {
-        System.out.println("inside drop method");
+
         Dragboard db = event.getDragboard();
-
         Node node = (Node) event.getSource();
+        Pane pane;
 
-        System.out.println("X: " + GridPane.getColumnIndex(node));
-        System.out.println("Y: " + GridPane.getRowIndex(node));
-        System.out.println(node.getParent());
+        System.out.print("Drop on cell: ");
+        System.out.print("X:" + GridPane.getColumnIndex(node));
+        System.out.println(" Y:" + GridPane.getRowIndex(node));
 
-//        boolean success = false;
-        Pane pane = (Pane) event.getTarget();
-        pane.getChildren().add(new ImageView(db.getImage()));
-//        ImageView image = new ImageView(db.getImage());
-//        if (db.hasImage()) {
-//            System.out.println(event.getTarget().getClass());
-//            event.setDropCompleted(true);
-//        }
+        if (event.getTarget() instanceof ImageView) {
+            ImageView temp = (ImageView) event.getTarget();
+            pane = (Pane) temp.getParent();
+        } else {
+            pane = (Pane) event.getTarget();
+        }
 
-//        Node node = event.getPickResult().getIntersectedNode();
-//        if(db.hasImage()){
-//
-//            Integer cIndex = GridPane.getColumnIndex(node);
-//            Integer rIndex = GridPane.getRowIndex(node);
-//            int x = cIndex == null ? 0 : cIndex;
-//            int y = rIndex == null ? 0 : rIndex;
-//            ImageView image = new ImageView(db.getImage());
-//
-//            grid.add(image, x, y);
-//            success = true;
-//        }
-//
-//event.setDropCompleted(success);
+        if (pane.getChildren().size() > 0) {
+            pane.getChildren().clear();
+        }
 
+        ImageView image = new ImageView(db.getImage());
+        image.setOnDragDetected(event1 -> {
+            ImageView image1 = (ImageView) event1.getSource();
+            Dragboard db1 = image1.startDragAndDrop(TransferMode.ANY);
 
+            ClipboardContent cbContent = new ClipboardContent();
+            cbContent.putImage(image1.getImage());
+            db1.setContent(cbContent);
+            image1.setVisible(false);
+            event1.consume();
+        });
+
+        pane.getChildren().add(image);
         event.consume();
     }
 
     @FXML
-    public void handleMouse(MouseEvent event) {
-
+    public void handleClick(MouseEvent event) {
         ImageView temp = (ImageView) event.getTarget();
         Node node = (Node) temp.getParent();
         System.out.println("X: " + GridPane.getColumnIndex(node));
         System.out.println("Y: " + GridPane.getRowIndex(node));
     }
-
-
 }
 
 
