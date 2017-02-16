4f0ad175b06fddc68d1bf5ff7043ea3661f0eced
bobnewmark <bobnewmark@ukr.net>
2017-02-16 11:08:39 +0200

    constants edited

--- client/src/main/java/chess/Constants.java
+++ client/src/main/java/chess/Constants.java
@@ -7,5 +7,5 @@ class Constants {
     static final String HOST = "localhost";
     static final int PORT = 2544;
     public static final int BOARDSIZE = 8;
-    public static final int TIME = 30;
+    static final int TIME = 30;
 }
